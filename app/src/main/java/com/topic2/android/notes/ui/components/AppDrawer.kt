62d2c72150073ed7cb0078bf26da46c3f59c68d5
gysar13 <134543481+gysar13@users.noreply.github.com>
2024-05-18 01:10:15 +0300

    AppDrawer.kt

--- app/src/main/java/com/topic2/android/notes/ui/components/AppDrawer.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/AppDrawer.kt
@@ -20,6 +20,7 @@ import com.topic2.android.notes.routing.NotesRouter
 import com.topic2.android.notes.routing.Screen
 import com.topic2.android.notes.theme.NotesThemeSettings
 
+
 @Composable
 private fun AppDrawerHeader(){
     Row(modifier = Modifier.fillMaxWidth()){
