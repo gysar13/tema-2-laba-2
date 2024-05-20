b625dd36517afc169d769695efdf6c8aca382630
gysar13 <134543481+gysar13@users.noreply.github.com>
2024-05-20 05:13:22 +0300

    1 NoteColor (red circle + border)

--- app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
@@ -1,13 +1,16 @@
 package com.topic2.android.notes.ui.components
 
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -18,6 +21,13 @@ fun NoteColor(){
             .size(40.dp)
             .clip(CircleShape)
             .background(Color.Red)
+            .border(
+                BorderStroke(
+                    2.dp,
+                    SolidColor(Color.Black)
+                ),
+                CircleShape
+            )
     )
 }
 
