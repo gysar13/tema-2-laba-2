9b6ea24bcedf5b3fc5c9e64142b023998b6b5158
gysar13 <134543481+gysar13@users.noreply.github.com>
2024-05-20 05:11:16 +0300

    1 NoteColor (yellow)

--- app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
@@ -3,8 +3,10 @@ package com.topic2.android.notes.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -12,7 +14,11 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun NoteColor(){
     Box(
-        modifier = Modifier.size(40.dp).background(Color.Red)
+        modifier = Modifier
+            .size(40.dp)
+            .background(Color.Red)
+            .clip(CircleShape)
+            .background(Color.Yellow)
     )
 }
 
